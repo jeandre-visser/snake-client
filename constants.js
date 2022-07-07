0fe61b89850ca44420a79772e24c4d3e35a37c6b
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-07 00:30:19 +0000

    Added constants and exported

---
+++ constants.js
@@ -0,0 +1,18 @@
+const IP = '165.227.47.243';
+const PORT = 50541;
+
+
+module.exports = {
+  IP,
+  PORT,
+  move_keys: {
+    w: 'up',
+    s: 'down',
+    a: 'left',
+    d: 'right'
+  },
+  message: {
+    m: 'HA',
+    n: 'LOL'
+  }
+};
